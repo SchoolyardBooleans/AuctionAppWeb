046340bbf187ae7b76b89621f75c775173a66fe2
Erik Owen <erikjowen@yahoo.com>
2015-01-12 15:00:18 -0800

    modifying the create auction page

--- public/javascripts/create_auction.js
+++ public/javascripts/create_auction.js
@@ -1,8 +1,36 @@
 $(document).ready(function() {
 
+	$('#dp_start').datepicker();
     $('#dp_end').datepicker();
 
-    $('#submit_auction').click(function() {
+    dropdown_title_changer("start_hour", "start_hour_btn");
+    dropdown_title_changer("start_minute", "start_minute_btn");
+    dropdown_title_changer("end_hour", "end_hour_btn");
+    dropdown_title_changer("end_minute", "end_minute_btn");
+
+ //    $(function() {
+	// 	$("#start_hour li a").click(function() {
+	// 		$("#start_hour_btn:first-child").text($(this).text() + ' ');
+ //    		$("#start_hour_btn:first-child").val($(this).text());
+ //    		$("#start_hour_btn:first-child").append('<span class="caret"></span>')
+
+	// 	});
+
+	// });
+
+    function dropdown_title_changer(id_name, id_btn_name) {
+    	$(function() {
+			$("#" + id_name + " li a").click(function() {
+				$("#" + id_btn_name + ":first-child").text($(this).text() + ' ');
+    			$("#" + id_btn_name + ":first-child").val($(this).text());
+    			$("#" + id_btn_name + ":first-child").append('<span class="caret"></span>')
+
+			});
+
+		});
+    }
+
+    // $('#submit_auction').click(function() {
     	// var name = $("#name").val();
     	// var email = $('#email').val();
     	// var body = $('#body').val();
@@ -22,5 +50,5 @@ $(document).ready(function() {
     	// 		}
 	    //     });
     	// }
-    });
+    // });
 });
