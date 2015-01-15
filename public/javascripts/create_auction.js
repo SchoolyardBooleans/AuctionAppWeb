98305e3f93ffb3185935c5d7ee4ddd67927dfed8
Erik Owen <erikjowen@yahoo.com>
2015-01-15 00:11:03 -0800

    create a new auction start and end times now default to today and tomorrow

--- public/javascripts/create_auction.js
+++ public/javascripts/create_auction.js
@@ -3,24 +3,14 @@ $(document).ready(function() {
 	$('#dp_start').datepicker();
     $('#dp_end').datepicker();
 
+    setDefaultDates();
+
     dropdown_title_changer("start_hour", "start_hour_btn");
     dropdown_title_changer("start_minute", "start_minute_btn");
     dropdown_title_changer("end_hour", "end_hour_btn");
     dropdown_title_changer("end_minute", "end_minute_btn");
     dropdown_title_changer("start_am_pm", "start_am_pm_btn");
     dropdown_title_changer("end_am_pm", "end_am_pm_btn");
-
-    function dropdown_title_changer(id_name, id_btn_name) {
-    	$(function() {
-			$("#" + id_name + " li a").click(function() {
-				$("#" + id_btn_name + ":first-child").text($(this).text() + ' ');
-    			$("#" + id_btn_name + ":first-child").val($(this).text());
-    			$("#" + id_btn_name + ":first-child").append('<span class="caret"></span>')
-
-			});
-
-		});
-    }
 
 	/*Validate the form*/
     $('#create_auction').bootstrapValidator(
@@ -38,25 +28,63 @@ $(document).ready(function() {
 	    }
 	});
 
-    // $('#submit_auction').click(function() {
-    	// var name = $("#name").val();
-    	// var email = $('#email').val();
-    	// var body = $('#body').val();
-
-    	// if(email.length <= 0) {
-	    //     $.ajax({
-	    //         type:'POST',
-	    //         url:'/erik/contact/submission',
-	    //         data: {'name' : name, 'body' : body},
-	    //         dataType: 'JSON',
-	    //         complete: function(data) {
-					// //$('#submit_button').before('<span id="notifier"><font color="#00FF00">Message sent ✓</font></span>');
-					// $('#notifier').hide();
-					// $('#notifier').fadeIn(600);
-
-					// $("#submit_button").prop("disabled", true);
-    	// 		}
-	    //     });
-    	// }
-    // });
 });
+
+function dropdown_title_changer(id_name, id_btn_name) {
+	$(function() {
+		$("#" + id_name + " li a").click(function() {
+			$("#" + id_btn_name + ":first-child").text($(this).text() + ' ');
+			$("#" + id_btn_name + ":first-child").val($(this).text());
+
+		});
+
+	});
+}
+
+function prettifyDateString(month, day, year) {
+	if(day < 10) {
+    	day = '0'+ day;
+	} 
+
+	if(month < 10) {
+    	month = '0' + month;
+	} 
+
+	return month + '/' + day + '/' + year;
+}
+
+function setDefaultDates() {
+	var today = new Date();
+	var day = today.getDate();
+	var month = today.getMonth() + 1; //January is 0!
+	var year = today.getFullYear();
+
+	var tomorrow = new Date(today.getTime() + 24 * 60 * 60 * 1000);
+	var tDay = tomorrow.getDate();
+	var tMonth = tomorrow.getMonth() + 1;
+	var tYear = tomorrow.getFullYear();
+
+	var todayStr = prettifyDateString(month, day, year);
+	var tomorrowStr = prettifyDateString(tMonth, tDay, tYear);
+
+	$('#dp_start').text(todayStr);
+	$('#dp_start').val(todayStr);
+	$('#dp_start').attr("placeholder", todayStr);
+	$('#start_hour_btn').text('12');
+	$('#start_hour_btn').val('12');
+	$('#start_minute_btn').text('00');
+	$('#start_minute_btn').val('00');
+	$('#start_am_pm_btn').text('PM');
+	$('#start_am_pm_btn').val('PM');
+
+	$('#dp_end').text(tomorrowStr);
+	$('#dp_end').val(tomorrowStr);
+	$('#dp_end').attr("placeholder", tomorrowStr);
+	$('#end_hour_btn').text('12');
+	$('#end_hour_btn').val('12');
+	$('#end_minute_btn').text('00');
+	$('#end_minute_btn').val('00');
+	$('#end_am_pm_btn').text('PM');
+	$('#end_am_pm_btn').val('PM');
+
+}
