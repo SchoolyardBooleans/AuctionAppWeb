8ff0096273456cee0603cbe8c549b7c30b25a72c
Erik Owen <erikjowen@yahoo.com>
2015-01-21 17:49:31 -0800

    create auction now has feedback if form submitted successfully or not

--- routes/create_auction.js
+++ routes/create_auction.js
@@ -24,69 +24,32 @@ router.get('/', function(req, res) {
 
 /* POST created auction */
 router.post('/', function(req, res) {
-	console.log("Auction posted");
 	var conn = new jsforce.Connection({
 		accessToken: req.session.accessToken,
 		instanceUrl: req.session.instanceUrl
 	});
-	console.log("Auction posted 2");
+
 	var start_date = new Date(req.body.start_date).toISOString(),
 		end_date = new Date(req.body.end_date).toISOString();
 
-		console.log("Auction posted 3");
-
+	/*Still need to add on a location*/
 	var auction = {
 		Hosting_Nonprofit__c : 'a0Zj0000000eDTTEA2',
 		name : req.body.name,
 		Start_Time__c : start_date,
 		End_Time__c : end_date
 	}
-	console.log("Auction posted 4");
 
 	conn.sobject('Auction__c').create(auction, function(err, ret) {
 		if (err || !ret.success) {
+			res.status(406).end();
 			return console.error(err, ret);
 		}
 
   		console.log("Created record id : " + ret.id);
-  		res.end();
+  		res.status(200).end();
 	});
 
-	console.log("Auction posted 5");
-
-	// var body = {
-	// 	name: req.body.name,
-	// 	orgId: 'a0Zj0000000eDTTEA2', 
-	// 	location: req.body.location,
-	// 	startDate: req.body.date
-	// };
-
-	// console.log("Req body %j", req.body);
-
-	// request.post(
-	//     'https://schooolyardbooleans-developer-edition.na16.force.com/public/services/apexrest/auctions',
-	//     {
-	//     	json: {
-	//     		name: req.body.name,
-	//     		orgId: 'a0Zj0000000eDTTEA2',
-	//     		location: req.body.location,
-	// 			startDate: req.body.start_date,
-	// 			endDate: req.body.end_date
-	// 		}
-	// 	},
-	//     function (error, response, body) {
-	//         if (!error && response.statusCode == 200) {
-	//             console.log(body);
-	//         }
-	//         else {
-	//         	console.log('Error: ' + error + '\n' + 'Response code: ' + response.statusCode + '\n' + 'Body: ' + body);
-
-	//         	/*Need to notify that auction was not created somehow*/
-	//         }
-
-	//         res.end();
-	//     }
-	// );
 });
 
 
