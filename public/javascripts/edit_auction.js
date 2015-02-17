dc6185d79a25facf89c83e462b9eacfe8bf94268
Erik Owen <erikjowen@yahoo.com>
2015-02-17 11:01:45 -0800

    started working on edit item page

--- public/javascripts/edit_auction.js
+++ public/javascripts/edit_auction.js
@@ -1,104 +1,106 @@
 $(document).ready(function() {
+    initializeValidation()
+	initialize();
+    initializeClickListeners();
 
-	/*Validate the form*/
+});
+
+function initializeValidation() {
     $('#edit_auction').formValidation({
-	    framework: 'bootstrap',
-	    icon: {
+        framework: 'bootstrap',
+        icon: {
             valid: 'glyphicon glyphicon-ok',
             invalid: 'glyphicon glyphicon-remove',
             validating: 'glyphicon glyphicon-refresh'
         },
-		fields: {
-	    	auction_name: {
-	        	message: 'The auction name is not valid',
-	        	validators: {
-	            	notEmpty: {
-	                	message: 'An auction name is required'
-	            	}
-	        	}
-	    	},
-	    	start_date_input: {
-	        	message: "The auction's start date is not valid",
-	        	validators: {
-	            	notEmpty: {
-	                	message: "The auction's start date is required"
-	            	}
-	        	}
-	    	},
-	    	end_date_input: {
-	        	message: "The auction's end date is not valid",
-	        	validators: {
-	            	callback: {
+        fields: {
+            auction_name: {
+                message: 'The auction name is not valid',
+                validators: {
+                    notEmpty: {
+                        message: 'An auction name is required'
+                    }
+                }
+            },
+            start_date_input: {
+                message: "The auction's start date is not valid",
+                validators: {
+                    notEmpty: {
+                        message: "The auction's start date is required"
+                    }
+                }
+            },
+            end_date_input: {
+                message: "The auction's end date is not valid",
+                validators: {
+                    callback: {
                         message: "The auction's end date must be after the start date",
                         callback: function(value, validator, $field) {
                             var start_str = $('#start_date_input').val(),
-                            	end_str = $('#end_date_input').val();
+                                end_str = $('#end_date_input').val();
 
                             var start = new Date(start_str),
-                            	end = new Date(end_str);
+                                end = new Date(end_str);
 
                             console.log('start: ' + start.getTime() + ' , end ' + end.getTime());
 
                             return start.getTime() < end.getTime();
                         }
                     },
-					notEmpty: {
-	                	message: "The auction's end date is required"
-	            	}
-	        	}
-	    	},
-	    	auction_location: {
-	        	message: "The auction's location is not valid",
-	        	validators: {
-	            	notEmpty: {
-	                	message: 'An auction location is required'
-	            	}
-	        	}
-	    	}
-	    }
-	}).on('success.form.fv', function(e) {
+                    notEmpty: {
+                        message: "The auction's end date is required"
+                    }
+                }
+            },
+            auction_location: {
+                message: "The auction's location is not valid",
+                validators: {
+                    notEmpty: {
+                        message: 'An auction location is required'
+                    }
+                }
+            }
+        }
+    }).on('success.form.fv', function(e) {
         /*Called when form button is pressed, and form is validated*/
         
         e.preventDefault();
 
         var name = $("#auction_name").val(),
-    		id = $('#auction_id').text(),
-    		start_date = $('#start_date_input').val(),
-    		end_date = $('#end_date_input').val(),
-    		location = $('#auction_location').val();
+            id = $('#auction_id').text(),
+            start_date = $('#start_date_input').val(),
+            end_date = $('#end_date_input').val(),
+            location = $('#auction_location').val();
 
-    	console.log('name: ' + name);
-    	console.log('id: ' + id);
+        console.log('name: ' + name);
+        console.log('id: ' + id);
 
         $.ajax({
             type:'POST',
             url:'/edit_auction',
             data: {
-            	'name': name,
-            	'id': id,
-            	'start_date': start_date,
-            	'end_date': end_date,
-            	'location': location
+                'name': name,
+                'id': id,
+                'start_date': start_date,
+                'end_date': end_date,
+                'location': location
             },
             dataType: 'JSON',
             complete: function(data) {
-            	if(data.status == 200) {
-            		console.log('Auction updated successfully')
-            		$('#notifier').html('<h4 style="color:#008A00">Auction Updated ✓</h4>');
-            	}
-            	else {
-            		console.log('not saved')
-            		$('#notifier').html('<h4 style="color:red">Unable To Update Auction</h4>');
-            	}
-				$('#notifier').hide();
-				$('#notifier').fadeIn(600);
-			}
+                if(data.status == 200) {
+                    console.log('Auction updated successfully')
+                    $('#notifier').html('<h4 style="color:#008A00">Auction Updated ✓</h4>');
+                }
+                else {
+                    console.log('not saved')
+                    $('#notifier').html('<h4 style="color:red">Unable To Update Auction</h4>');
+                }
+                $('#notifier').hide();
+                $('#notifier').fadeIn(600);
+            }
         });
     });
-
-	initialize();
-
-});
+}
 
 function initialize() {
 	$('#notifier').hide();
@@ -124,4 +126,11 @@ function initialize() {
     	$('#edit_auction').data('formValidation').updateStatus('end_date_input', 'NOT_VALIDATED').validateField('end_date_input');
     	console.log('end date changed 2');
 	});
+}
+
+function initializeClickListeners() {
+    $(".clickableRow").click(function() {
+        console.log("Row clicked");
+        window.document.location = $(this).attr("href");
+    });
 }
--- routes/edit_auction.js
+++ routes/edit_auction.js
@@ -1,7 +1,5 @@
 var express = require('express');
 var jsforce = require('jsforce');
-// won't be needed later
-var request = require('request');
 var util = require('util');
 var moment = require('moment');
 var router = express.Router();
@@ -16,8 +14,6 @@ router.get('/:id', function(req, res) {
 	console.log('in edit auction');
 	var auction_id = req.params.id;
 	var query_str = "SELECT Id, Name, Start_Time__c, End_Time__c, Location__r.Name," +
-		//"(SELECT Name FROM Auctions__r), " +
-		//"Auction_Location__r.Name," + 
 		"(SELECT Name, Id, Description__c, Estimated_Value__c FROM Auction_Items__r) FROM Auction__c WHERE Id = '" + auction_id + "'";
 
 	conn.query(query_str, function(err, auction) {
@@ -54,39 +50,6 @@ router.get('/:id', function(req, res) {
 
 		res.render('edit_auction', dustVars);
 	});
-
-	// conn.sobject('Auction__c').retrieve(auction_id, function(err, auction) {
-	//  	if (err) {
-	//  		return console.error(err);
-	//  	}
-
-	//  	var start_str = moment(auction.Start_Time__c).format('MM/DD/YYYY hh:SS A');
-	//  	var end_str = moment(auction.End_Time__c).format('MM/DD/YYYY hh:SS A');
-
-	// 	/*still needs auction location*/
-	// 	var dustVars = {
-	// 		title: 'Edit Auction',
-	// 		auction_name: auction.Name,
-	// 		auction_id: auction_id,
-	// 		auction_start_date: start_str,
-	// 		auction_end_date: end_str,
-	// 		cssFiles: [
-	// 			{css: 'edit_auction.css'},
-	// 			{css: 'formValidation.min.css'},
-	// 			{css: 'bootstrap-datetimepicker.min.css'}
-	// 		],
-	// 		javascriptFiles: [
-	// 			{javascript: 'bootstrap-datetimepicker.min.js'},
-	// 			{javascript: 'formValidation.min.js'},
-	// 			{javascript: 'formValidation-bootstrap.min.js'},
-	// 			{javascript: 'edit_auction.js'}
-	// 		]
-	// 	};
-
-	// 	console.log('Here is auction object: ' + util.inspect(auction, false, null));
-
-	// 	res.render('edit_auction', dustVars);
-	// });
 });
 
 /* POST edited auction */
@@ -230,6 +193,43 @@ router.post('/:id/add_item', function(req, res) {
 	  		console.log("Created record id : " + ret.id);
 	  		res.status(200).redirect('/edit_auction/' + req.params.id);
 		});
+	});
+});
+
+/*Get add an item to auction page*/
+router.get('/:auction_id/edit_item/:item_id', function(req, res) {
+	var conn = new jsforce.Connection({
+		accessToken: req.session.accessToken,
+		instanceUrl: req.session.instanceUrl
+	});
+
+	var auction_id = req.params.auction_id,
+		item_id = req.params.item_id;
+
+	conn.sobject('Auction_Item__c').retrieve(item_id, function(err, item) {
+		if (err) {
+			return console.error(err, ret);
+		}
+
+		console.log(util.inspect(item, false, null));
+
+		var dustVars = {
+			title: 'Edit Item',
+			cssFiles: [
+				{css: 'add_item.css'},
+				{css: 'bootstrap-multiselect.css'},
+				{css: 'formValidation.min.css'}
+			],
+			javascriptFiles: [
+				{javascript: 'bootstrap-filestyle.min.js'},
+				{javascript: 'bootstrap-multiselect.js'},
+				{javascript: 'formValidation.min.js'},
+				{javascript: 'formValidation-bootstrap.min.js'},
+				{javascript: 'add_item.js'}
+			]
+		};
+
+  		res.render('add_item', dustVars);
 	});
 });
 
