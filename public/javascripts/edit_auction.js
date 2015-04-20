7e1e25d340feb6a85977350e807285259e3bf089
Erik Owen <erikjowen@yahoo.com>
2015-04-20 11:26:36 -0700

    created UI for deleting items

--- public/javascripts/edit_auction.js
+++ public/javascripts/edit_auction.js
@@ -136,6 +136,46 @@ function initialize() {
     	$('#edit_auction').data('formValidation').updateStatus('end_date_input', 'NOT_VALIDATED').validateField('end_date_input');
     	console.log('end date changed 2');
 	});
+
+    $(".delete_button a").on('click', function(ev) {
+        ev.preventDefault();
+        console.log("delete clicked");
+
+    });
+
+    $('#delete_modal').on('show.bs.modal', function(event) {
+
+        var auction_id = $(event.relatedTarget).closest('tr').data('id'),
+            auction_name = $(event.relatedTarget).closest('tr').data('name');
+
+        console.log("auction id is: " + auction_id);
+        console.log("auction name is: " + auction_name);
+
+        $('.modal-body').text('Are you sure you want to delete the item "' + auction_name + '"? This action cannot be undone.');
+
+        $("#confirm_delete").on('click', function(ev) {
+            ev.preventDefault();
+
+            // $.ajax({ 
+            //     type:'DELETE', 
+            //     url: '/delete_auction/' + auction_id, 
+            //     dataType: 'json',
+            //     complete: function(data) {
+            //         if(data.status == 200) {
+            //             $('tr[data-id="'+auction_id +'"]').remove();
+            //             console.log('item deleted');
+
+            //         }
+            //         else {
+            //             alert('Something went wrong. Auction could not be deleted.');
+            //         }
+
+            //         $('#delete_modal').modal('hide');
+            //     }
+
+            // });
+        });
+    });
 }
 
 function initializeClickListeners() {
